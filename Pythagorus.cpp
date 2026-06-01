4d38bb4f05ca375333203cce6413f226d119290e
Nattasit <nlhtwolf2008@gmail.com>
2026-06-01 17:52:16 +0700

    Organize

---
+++ Pythagorus.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    double n,m;
+    cin >> n >> m;
+    cout << fixed << setprecision(6) << sqrt((n*n) + (m*m));
+
+    
+    return 0;
+}
